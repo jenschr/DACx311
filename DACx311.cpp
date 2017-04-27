5b29e1afa2ca46e7a448ce96f3095acf209a01eb
jenschr <jenschr@gmail.com>
2017-04-27 08:30:10 +0200

    Tested and verified classes

---
+++ DACx311.cpp
@@ -0,0 +1,102 @@
+#include "DACxx11.h"
+#include <SPI.h>
+
+#if defined (SPI_HAS_TRANSACTION)
+  static SPISettings mySPISettings;
+#elif defined (__AVR__)
+  static uint8_t SPCRbackup;
+  static uint8_t mySPCR;
+#endif
+
+DACxx11::DACxx11(){}
+
+void DACxx11::begin( byte csPin, byte chip, float maxVoltage = 3.3 )
+{
+  _cs = csPin;
+  _chip = chip;
+  _maxVoltage = maxVoltage;
+
+  if( _chip == DAC5311 )
+  {
+    bits_to_write = 8;
+    bit_shift = 6;
+  } else if( _chip == DAC6311 )
+  {
+    bits_to_write = 10;
+    bit_shift = 4;
+  } else if( _chip == DAC7311 )
+  {
+    bits_to_write = 12;
+    bit_shift = 2;
+  } else {
+    
+  }
+  
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);
+  csport    = portOutputRegister(digitalPinToPort(_cs));
+  cspinmask = digitalPinToBitMask(_cs);
+
+#if defined (SPI_HAS_TRANSACTION)
+  SPI.begin();
+  mySPISettings = SPISettings(8000000, MSBFIRST, SPI_MODE0);
+#elif defined (__AVR__)
+  SPCRbackup = SPCR;
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.setDataMode(SPI_MODE0);
+  mySPCR = SPCR; // save our preferred state
+  SPCR = SPCRbackup;  // then restore
+#elif defined (__SAM3X8E__)
+  SPI.begin();
+  SPI.setClockDivider(21); //4MHz
+  SPI.setDataMode(SPI_MODE0);
+#endif
+
+  _singleVoltageStep = float(5.0) / pow( 2, bits_to_write );
+}
+
+void DACxx11::setVoltage( float voltage )
+{
+  // keep within limits
+  if( voltage > _maxVoltage ){ voltage = _maxVoltage; }
+  else if( voltage < 0 ){ voltage = 0; }
+  
+  // calc and set voltage
+  uint16_t value = int(voltage/_singleVoltageStep);
+  value = value << bit_shift;
+  uint8_t upper = value >> 8;
+  uint8_t lower = value & 0xff;
+  writedata(upper,lower);
+}
+
+inline void DACxx11::writedata(uint8_t upper, uint8_t lower)
+{
+  *csport &= ~cspinmask;
+  
+  #if defined (SPI_HAS_TRANSACTION)
+    SPI.beginTransaction(mySPISettings);
+#endif
+
+#if defined (SPI_HAS_TRANSACTION)
+  SPI.transfer(upper);
+  SPI.transfer(lower);
+#elif defined (__AVR__)
+  SPCRbackup = SPCR;
+  SPCR = mySPCR;
+  SPI.transfer(upper);
+  SPI.transfer(lower);
+  SPCR = SPCRbackup;
+#elif defined (__arm__)
+  SPI.setClockDivider(21); //4MHz
+  SPI.setDataMode(SPI_MODE0);
+  SPI.transfer(upper);
+  SPI.transfer(lower);
+#endif
+  
+#if defined (SPI_HAS_TRANSACTION)
+  SPI.endTransaction();
+#endif
+  
+  *csport |= cspinmask;
+}
---
+++ DACx311.h
@@ -0,0 +1,78 @@
+/***************************************************
+  This library provides simple usage of the DACx311 chips
+  from TI. These are single- channel, voltage output
+  digital-to-analog converter and great for setting a
+  voltage reference. DAC5311, DAC3311 and DAC7311 are
+  supported, but only DAC7311 is tested.
+
+  Jens Chr Brynildsen April 2017 (MIT license)
+  
+  Multi-MCU SPI code for arm & AVR was borrowed
+  from Adafruit's excellent ST7735 lib:
+  https://github.com/adafruit/Adafruit-ST7735-Library
+  
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit! http://www.adafruit.com
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+#ifndef DACXX11_H
+#define DACXX11_H
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+ #include "Print.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#if defined(__SAM3X8E__)
+  #include <include/pio.h>
+  #define PROGMEM
+  #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+  #define pgm_read_word(addr) (*(const unsigned short *)(addr))
+  typedef unsigned char prog_uchar;
+#elif defined(__AVR__)
+  #include <avr/pgmspace.h>
+#elif defined(ESP8266)
+  #include <pgmspace.h>
+#endif
+
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+// Select one of the IC's from below (sets the resolution)
+#define DAC5311 1
+#define DAC6311 2
+#define DAC7311 3
+
+class DACxx11 {
+  public:
+    DACxx11();
+    void begin( byte csPin, byte chip, float maxVoltage = 3.3 );
+    void setVoltage( float c );
+  private:
+    byte _chip;
+    void writedata(uint8_t upper, uint8_t lower);
+    uint8_t bits_to_write;
+    uint8_t bit_shift;
+    float _singleVoltageStep;
+    float _maxVoltage;
+
+    #if defined(__AVR__) || defined(CORE_TEENSY)
+    volatile uint8_t *csport;
+    uint8_t  _cs, cspinmask;
+  #elif defined(__arm__)
+    volatile RwReg  *csport;
+    uint32_t  _cs, cspinmask;
+    int32_t   _rst;  // Must use signed type since a -1 sentinel is assigned.
+  #endif
+};
+
+#endif /* DACXX11_H */
+
